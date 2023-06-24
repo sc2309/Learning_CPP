61229a2e077ac9d6e916d175e107ddfef650e071
aananyarai <134141562+aananyarai@users.noreply.github.com>
2023-06-24 18:12:36 +0530

    Add files via upload

---
+++ Fibonacci series.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci (int n)
+{
+    if (n==0)
+    {
+	   return 0;
+	}
+    
+    else if (n==1)
+    {
+       return 1;
+	}
+	
+	else { 
+	return (fibonacci(n-1) + fibonacci (n-2));
+		}
+}
+
+ int main ()
+{
+	int n,i;
+	cout<< "Enter the number of terms";
+    cin>>n;
+    cout<<"fibonacci series"<<endl;
+    
+    for(i=0; i<n; i++)
+    
+    { 
+	cout<<fibonacci (i)<<endl;
+    }
+    return 0;
+}
---
+++ Rectangle cpp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+	 for (int j=1; j<=10; j++) {
+	 	 for (int i=8; i>=0; i--) {
+	 	 	cout<< " ";
+	 	}
+	 	 	for (int k=1; k<=8; k++){
+			   cout<<"*";
+	}
+	
+}
+    
+        cout<<endl;
+    
+  
+return 0;
+}
